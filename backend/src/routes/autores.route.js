28441a977372fbed68fff6c0debc92c77f5482f9
rodrigodev.ts <rodrigodev.ts@gmail.com>
2025-05-21 21:31:27 -0500

    sacar auth de autores

--- backend/src/routes/autores.route.js
+++ backend/src/routes/autores.route.js
@@ -2,24 +2,24 @@
 const express = require('express');
 const router = express.Router();
 const autoresController = require('../controllers/autores.controller');
-const { authMiddleware } = require('../../auth/auth');
+//const { authMiddleware } = require('../../auth/auth');
 
 // Ruta para crear un nuevo autor
-router.post('/crear_autor', authMiddleware, autoresController.crearAutor);
+router.post('/crear_autor', autoresController.crearAutor);
 
 // Ruta para listar todos los autores
-router.get('/', authMiddleware, autoresController.obtenerAutores);
+router.get('/', autoresController.obtenerAutores);
 
 // Ruta para obtener un autor específico por ID
-router.get('/:id', authMiddleware, autoresController.obtenerAutorPorId);
+router.get('/:id', autoresController.obtenerAutorPorId);
 
 // Ruta para obtener los libros de un autor específico
-router.get('/:id/libros', authMiddleware, autoresController.obtenerLibrosDeAutor);
+router.get('/:id/libros', autoresController.obtenerLibrosDeAutor);
 
 // Ruta para actualizar un autor
-router.put('/actualizar/:id', authMiddleware, autoresController.actualizarAutor);
+router.put('/actualizar/:id', autoresController.actualizarAutor);
 
 // Ruta para eliminar un autor
-router.delete('/:id', authMiddleware, autoresController.eliminarAutor);
+router.delete('/:id', autoresController.eliminarAutor);
 
 module.exports = router;
